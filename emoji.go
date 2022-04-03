fc1529ee488db55ff15cb40a76666f54ee2dea3a
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-04-03 11:32:01 +0200

    ✨ print the emoticon code block

--- emoji.go
+++ emoji.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+const hrule = "----------------------------------------------------------------"
+
 func main() {
 	fmt.Println("😀")
+	fmt.Println(hrule)
+
+	for r := 0x1F600; r <= 0x1F64F; r++ {
+		fmt.Printf("%c ", r)
+	}
+	fmt.Println()
+	fmt.Println(hrule)
+
 }
